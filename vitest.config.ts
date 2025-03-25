3ff7c0d5e568a20b31f3e633527090a3446dec73
Ville Reippainen <ville.reippainen@wunderdog.fi>
2025-03-25 19:33:36 +0200

    Upgrade vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
-      exclude: ['**/node_modules/**', '**/dist/**', '**/__tests__/**', '**/__mocks__/**'],
+      exclude: ['**/node_modules/**', '**/dist/**', '**/__tests__/**', '**/__mocks__/**', '*.config.*', 'src/types/**'],
     },
   },
 });
